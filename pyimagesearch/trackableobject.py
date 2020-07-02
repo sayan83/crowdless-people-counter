acfddced60f310d28ff3eaacfb5369ecbc2413fd
Sayantan <sayanbis83@gmail.com>
2020-07-02 18:11:12 +0530

    People Counter v0.1

---
+++ pyimagesearch/trackableobject.py
@@ -0,0 +1,11 @@
+class TrackableObject:
+	def __init__(self, objectID, centroid):
+		# store the object ID, then initialize a list of centroids
+		# using the current centroid
+		self.objectID = objectID
+		self.centroids = [centroid]
+		self.direction = ''
+		# initialize a boolean used to indicate if the object has
+		# already been counted or not
+		self.counted = False
+		
